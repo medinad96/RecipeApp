251ba8d3e364631afbfa31464abb020fc4def6fd
Macbook <macbook@Mac.local>
2021-01-23 15:58:52 -0500

    Change routing for home page

--- RecipeApp/recipe_api/views.py
+++ RecipeApp/recipe_api/views.py
@@ -3,7 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def main(request):
-    return HttpResponse("<h1>Hello World</h1>")
-
-
-
+    return render(request, 'recipe_api/home.html')
